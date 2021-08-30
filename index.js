aaac33828573611a2608583e9398c30fbf5cc148
Rohan <rohank2502@gmail.com>
2021-08-30 14:56:43 +0530

    added 1 command for socail

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 const Discord = require("discord.js");
 const dotenv = require('dotenv');
-
 dotenv.config();
-
-// const intents = new Discord.Intents(32767);
-// const client = new Discord.Client({ intents });
 const client = new Discord.Client();
 
 client.once("ready", () => {
-  console.log('Logged');
+  console.log('BOT IS ONLINE');
 })
 
 client.on("guildMemberAdd", (client, member) => {
-  console.log(client)
   const channel = client.guild.channels.cache.find(
     c => c.name == "general"
   );
-
   if (!channel) return;
-
   const embed = new Discord.MessageEmbed();
-
   embed
     .setTitle("New Member in Community")
     .setColor("GREEN")
@@ -36,8 +28,42 @@ client.on("guildMemberAdd", (client, member) => {
     )
     .setTimestamp(client.joinedTimestamp)
     .setFooter('We are glad to have you with us', 'https://i.imgur.com/eRxIacv.png');
-
   channel.send(embed);
+});
+
+
+client.on('message', message => {
+  if (message.channel.name === 'bot-playground') {
+    if (message.content === '/social') {
+      const embed = new Discord.MessageEmbed();
+      embed
+        .setTitle("OUR SOCIALS")
+        .setColor("GREEN")
+        .setAuthor('GDSC_SITS')
+        .setThumbnail(`https://i.imgur.com/eRxIacv.png`)
+        .setImage('https://i.imgur.com/JCHJgfq.png')
+        .addFields(
+          {
+            name: "LinkedIn",
+            value: 'https://www.linkedin.com/company/dsc-sits/',
+            inline: false
+          },
+          {
+            name: "Twitter",
+            value: 'https://twitter.com/gdsc_sits',
+            inline: false
+          },
+          {
+            name: "Instagram",
+            value: 'https://www.instagram.com/gdsc_sits/',
+            inline: false
+          }
+        )
+        .setTimestamp(client.joinedTimestamp)
+        .setFooter('Follow us on Our Socials and you will never miss any update', 'https://i.imgur.com/eRxIacv.png');
+      message.channel.send(embed);
+    }
+  }
 });
 
 
